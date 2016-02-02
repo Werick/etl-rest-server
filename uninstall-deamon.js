8a99837ef8a837ab589d98ecdc44e2b2287032e6
werick <jackercmugoma@gmail.com>
2016-02-02 17:13:50 +0300

    merge conflicts

--- uninstall-deamon.js
+++ uninstall-deamon.js
@@ -8,8 +8,8 @@ var svc = new Service({
 
 // Listen for the "uninstall" event so we know when it's done.
 svc.on('uninstall', function() {
-    console.log('Uninstall complete.');
-    console.log('The service exists: ', svc.exists);
-  });
+  console.log('Uninstall complete.');
+  console.log('The service exists: ', svc.exists);
+});
 // Uninstall the service.
 svc.uninstall();
